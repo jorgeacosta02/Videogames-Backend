baf5a966960bf17ecbffe03294a9b548e5551dcd
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-09 17:34:45 -0300

    db

--- src/db.js
+++ src/db.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-  logging: false,
-  native: false,
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+    {
+      logging: false,
+      native: false,
+    }
+);
 
 // const sequelize = new Sequelize(DB_DEPLOY, {
 //   logging: false,

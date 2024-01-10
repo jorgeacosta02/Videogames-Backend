0b2bcd74fd9017cf82848e1596b59e522444f322
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-10 12:25:52 -0300

    comment conn in index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+// const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
 
-conn.sync({ alter: true }).then(() => {
+// conn.sync({ alter: true }).then(() => {
   server.listen(port, () => {
     console.log(`Listening at port ${port}`);
   });
-});
+// });
